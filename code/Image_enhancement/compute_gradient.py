a2f94d70a5564b1807ac689bfa8062f0373b406b
ligia26 <ligia-roxana.micu@kcl.ac.uk>
2021-09-26 20:31:07 +0200

    compute gradient fixed but warning

--- code/Image_enhancement/compute_gradient.py
+++ code/Image_enhancement/compute_gradient.py
@@ -7,7 +7,7 @@ def compute_gradient(image):
     filter2 = np.array([[1,2,1], [0,0,0], [-1,-2,-1]])
     Gx = signal.convolve2d(image, filter1)
     Gy = signal.convolve2d(image, filter2)
-    im_magnitude = sqrt(Gx**2+Gy**2)
+    im_magnitude = np.sqrt(Gx**2+Gy**2)
     im_direction = (np.tan(Gx/Gy))**(-1)                   
     return Gx, Gy, im_magnitude,im_direction
 
