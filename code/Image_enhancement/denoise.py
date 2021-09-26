d609f3e231d83da5313dcfa68ec20d58a43b5e10
Joyce den Hertog <joycedenhertog@live.nl>
2021-09-26 19:47:38 +0200

    add stuff on denoising

--- code/Image_enhancement/denoise.py
+++ code/Image_enhancement/denoise.py
@@ -1,10 +1,18 @@
-def denoise( image, kernel_type)
+import cv2
+import matplotlib.pyplot as plt
+
+def denoise(image, kernel_type, **kwargs):
     if kernel_type == 'box':
-        print('Not implemented')
+        imOu = cv2.blur(image, kwargs['ksize'])
     elif kernel_type == 'median':
-        print('Not implemented')
+        imOu = cv2.medianBlur(image, kwargs['ksize'])
     elif kernel_type == 'gaussian':
-        print('Not implemented')
+        imOu = cv2.GaussianBlur(image, kwargs['ksize'], 0)
     else:
         print('Operation not implemented')
     return imOu
+
+
+    
+
+
