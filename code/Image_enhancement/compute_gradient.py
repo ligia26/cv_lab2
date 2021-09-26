a64d5cab4a266bf14d6d8ff9a3abb522b734e162
ligia26 <ligia-roxana.micu@kcl.ac.uk>
2021-09-26 20:04:31 +0200

    gradient computing

--- code/Image_enhancement/compute_gradient.py
+++ code/Image_enhancement/compute_gradient.py
@@ -1,5 +1,14 @@
+from math import sqrt
+from scipy import signal
+import numpy as np
+
 def compute_gradient(image):
-    print('Not implemented\n')
+    filter1 = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    filter2 = np.array([[1,2,1], [0,0,0], [-1,-2,-1]])
+    Gx = signal.convolve2d(image, filter1)
+    Gy = signal.convolve2d(image, filter2)
+    im_magnitude = sqrt(Gx**2+Gy**2)
+    im_direction = (np.tan(Gx/Gy))**(-1)                   
     return Gx, Gy, im_magnitude,im_direction
 
 
