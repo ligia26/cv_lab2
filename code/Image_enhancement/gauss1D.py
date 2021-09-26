2936452cd9d353ea227277b2d88213642959f128
Joyce den Hertog <joycedenhertog@live.nl>
2021-09-26 18:21:39 +0200

    add gauss 1d, 2d and psnr

--- code/Image_enhancement/gauss1D.py
+++ code/Image_enhancement/gauss1D.py
@@ -1,9 +1,15 @@
 import numpy as np
+from math import sqrt, pi, exp
 
-def gauss1D( sigma , kernel_size )
-    G = np.zeros((1, kernel_size))
-    if kernel_size % 2 == 0
+def gauss1D( sigma , kernel_size ):
+    if kernel_size % 2 == 0:
         raise ValueError('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
-    # solution
-	
-	return G
+    
+    truncate_range = range(-int(kernel_size/2), int(kernel_size/2+1))
+    X = [(1 / (sigma * sqrt(2 * pi))) * exp(-(x**2)/(2*sigma**2)) 
+        for x in truncate_range]
+
+    # normalize G to sum to 1
+    G =  np.asarray([np.asarray(X) / sum(X)])
+
+    return G
--- code/Image_enhancement/myPSNR.py
+++ code/Image_enhancement/myPSNR.py
@@ -1,3 +1,21 @@
+import math
+import numpy as np
+import cv2
+
 def myPSNR( orig_image, approx_image ):
-    print('Not implemented\n')
+    orig_image = orig_image.astype('float32')
+    approx_image = approx_image.astype('float32')
+
+    MSE = np.mean((orig_image - approx_image)**2)
+    I_max = orig_image.max()
+    PSNR = 20 * math.log10(I_max/math.sqrt(MSE))
+    
     return PSNR
+
+if __name__ == '__main__':
+    img1_saltpepper = cv2.imread('./images/image1_saltpepper.jpg')
+    img1_gaussian = cv2.imread('./images/image1_gaussian.jpg')
+    img1 = cv2.imread('./images/image1.jpg')
+
+    print("PSNR img1 + img1 saltpepper: ", myPSNR(img1, img1_saltpepper))
+    print("PSNR img1 + img1 gaussian: ", myPSNR(img1, img1_gaussian))
